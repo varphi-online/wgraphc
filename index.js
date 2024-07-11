39663cfe8c5f5d2b84f94fdee1ab3f3141ad0b22
varphi-online <nbash1984@gmail.com>
2024-07-10 21:20:45 -0700

    scanner is finished, though console logs need to be relegated to a debug state

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ import init from "./pkg/wgraphcal.js";
 let wasmInitialized = false;
 
 async function wasmInit() {
-	await init();
-	wasmInitialized = true;
+  await init();
+  wasmInitialized = true;
 }
 
 export async function ensureWasmInit() {
-	if (!wasmInitialized) {
-		await wasmInit();
-	}
+  if (!wasmInitialized) {
+    await wasmInit();
+  }
 }
 
-export async function scanner() {
-	await ensureWasmInit();
+export async function scanner(input) {
+  await ensureWasmInit();
 
-	let res = wasm.return_string();
-	console.log("promise", res);
-	return res;
+  let res = wasm.return_string(input);
+  console.log("promise", res);
+  return res;
 }
